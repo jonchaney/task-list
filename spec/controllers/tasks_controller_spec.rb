53e9b190c71230c5ce6cf2dff5e3d7107e1c63f7
Jonathan Chaney <jonchaney@gmail.com>
2018-05-10 17:55:50 -0700

    write specs for tasks controller

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,41 +5,46 @@ RSpec.describe Api::TasksController, type: :controller do
         allow_message_expectations_on_nil
     end
 
-    describe 'POST #create' do
     let(:group) { Group.create!(name: 'Errands') }
     let(:task) { Task.create!(name: 'Go to the Bank', completed_at: nil, group_id: group.id) }
 
-        describe 'GET #show' do
-            it 'renders the task as json' do
-                get :show, params: { id: task.id }
-                expect(response.content_type).to eq("application/json")
-                expect(response).to have_http_status(200)
-            end
-
-            it 'http status as 404 not found when task does not exist' do
-                get :show, params: { id: 65 }
-                expect(response).to have_http_status(404)
-            end
+    describe 'GET #show' do
+        it 'renders the task as json' do
+            get :show, params: { id: task.id }
+            expect(response.content_type).to eq("application/json")
+            expect(response).to have_http_status(200)
         end
 
-        describe 'POST #create' do
-            it 'renders the task as json' do
-                post :create, params: {task: { name: 'Go to the Bank', completed_at: nil, group_id: group.id  } }
-                expect(response.content_type).to eq("application/json")
-                expect(response).to have_http_status(200)
-                expect(JSON.parse(response.body)['name']).to eq('Go to the Bank')
-            end
-
-            it 'validates group_id exists' do
-                post :create, params: {task: { name: 'Go to the Bank' } }
-                expect(response.content_type).to eq("application/json")
-                expect(flash[:errors]).to eq(['Group must exist'])
-                expect(response).to have_http_status(422)
-            end
+        it 'http status as 404 not found when task does not exist' do
+            get :show, params: { id: 65 }
+            expect(response).to have_http_status(404)
         end
-
-
     end
+
+    describe 'POST #create' do
+        it 'renders the task as json' do
+            post :create, params: {task: { name: 'Go to the Bank', completed_at: nil, group_id: group.id  } }
+            expect(response.content_type).to eq("application/json")
+            expect(response).to have_http_status(200)
+            expect(JSON.parse(response.body)['name']).to eq('Go to the Bank')
+        end
+
+        it 'validates group_id exists' do
+            post :create, params: {task: { name: 'Go to the Bank' } }
+            expect(response.content_type).to eq("application/json")
+            expect(flash[:errors]).to eq(['Group must exist'])
+            expect(response).to have_http_status(422)
+        end
+    end
+
+    describe 'GET #index' do 
+        it 'renders task index as json' do 
+            
+        end 
+
+    end 
+
+
     
 end
 
